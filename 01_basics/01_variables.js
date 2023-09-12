80842708b0f7e47b6847aab05f9f77aa9b2a1416
shiva kumar <115865333+shivakumardas@users.noreply.github.com>
2023-09-12 07:12:58 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=12723
+let accountEmail ="shiva092@gmail.com"
+var accountPassword ="12392"
+accountCity ="jaipur"
+
+//accountId =2  //not allowed
+
+accountEmail="hs@gmail.com"
+accountPassword="22727"
+accountCity ="bemgaluru"
+let accountstate;
+
+console.log(accountId)
+
+/*
+        Prefer not to use var
+        because of issue in block scope and functional scope
+ */
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+///   alert(3+3)  //we are using node js ,not browser
+
+console.log(3
+        
+        +3)  //code redability should be high
+console.log("shiva")
+
+let name = "shiva"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigint will be used for maximam number
+//string => ""
+//boolean ==> true/false
+//null = standalone value
+//undefined ==>
+//symbol 
+
+//object
+
+console.log(typeof null);// object
+console.log(undefined); //undefined
